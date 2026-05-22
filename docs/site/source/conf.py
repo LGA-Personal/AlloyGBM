24a097b6a1f451e62b71196efb57c230db3db5a1
Logan Ashby <ashbylogan12@gmail.com>
2026-05-22 02:38:11 -0400

    chore: bump version to 0.10.6

--- docs/site/source/conf.py
+++ docs/site/source/conf.py
@@ -11,8 +11,8 @@ sys.path.insert(0, str(PYTHON_BINDINGS))
 project = "AlloyGBM"
 copyright = "2026, Logan Ashby"
 author = "Logan Ashby"
-release = "0.10.5"
-version = "0.10.5"
+release = "0.10.6"
+version = "0.10.6"
 
 extensions = [
     "sphinx.ext.duration",
